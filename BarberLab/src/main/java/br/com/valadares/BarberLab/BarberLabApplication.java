fec745d5c3a19eb92913dbed85e59c27a73497ef
Gabriel Valadares <silva44gabriel@gmail.com>
2025-08-14 17:16:43 -0300

    first commit

---
+++ BarberLab/src/main/java/br/com/valadares/BarberLab/BarberLabApplication.java
@@ -0,0 +1,13 @@
+package br.com.valadares.BarberLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberLabApplication.class, args);
+	}
+
+}
---
+++ BarberLab/src/test/java/br/com/valadares/BarberLab/BarberLabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valadares.BarberLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
