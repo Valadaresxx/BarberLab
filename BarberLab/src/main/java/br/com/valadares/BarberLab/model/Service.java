40d678e822668db1d2bb2b00c77361b16d573aaa
Gabriel Valadares <silva44gabriel@gmail.com>
2025-08-16 14:13:52 -0300

    feat: add attibute createAt to the Service

--- BarberLab/src/main/java/br/com/valadares/BarberLab/model/Service.java
+++ BarberLab/src/main/java/br/com/valadares/BarberLab/model/Service.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "services")
@@ -26,4 +27,13 @@ public class Service {
     private BigDecimal price;
     @Column(nullable = false)
     private Integer durationMinutes;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        if(this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+    }
 }
