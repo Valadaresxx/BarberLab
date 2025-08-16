ab5cfd692b31033fae1ce129b7efaf45ae9cee21
Gabriel Valadares <silva44gabriel@gmail.com>
2025-08-16 14:07:06 -0300

    feat: add attributes to the Service

--- BarberLab/src/main/java/br/com/valadares/BarberLab/model/Service.java
+++ BarberLab/src/main/java/br/com/valadares/BarberLab/model/Service.java
@@ -1,4 +1,29 @@
 package br.com.valadares.BarberLab.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "services")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Service {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private BigDecimal price;
+    @Column(nullable = false)
+    private Integer durationMinutes;
 }
