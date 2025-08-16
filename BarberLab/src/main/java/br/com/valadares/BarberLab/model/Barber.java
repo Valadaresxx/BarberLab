7a4378d924dc094ca9a7e8823023be39793441ea
Gabriel Valadares <silva44gabriel@gmail.com>
2025-08-16 13:55:56 -0300

    feat: add V2 migration and update Barber entity with new fields

--- BarberLab/src/main/java/br/com/valadares/BarberLab/model/Barber.java
+++ BarberLab/src/main/java/br/com/valadares/BarberLab/model/Barber.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -19,7 +20,22 @@ public class Barber {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
-    private String name;
-    @OneToMany
+    private String firstName;
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String phone;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+    @ManyToMany
     private List<Service> services;
+
+    @PrePersist
+    public void prePersist() {
+        if(this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+    }
+
 }
